1e8f61af9dd739e84236a6630d040f63b022ff74
Samuel Lopes <samlopes89@gmail.com>
2017-05-12 11:06:57 -0500

    Functioning Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,59 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(MAINLOOP)
+  @KBD
+  D=M
+  @PAINT
+  D;JNE
+  @CLEAR
+  0;JMP
+
+(PAINT)
+  @i
+  M=0
+  (PAINTFILL)
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    A=D
+    M=-1
+    @i
+    M=M+1
+    @8192
+    D=A
+    @i
+    D=D-M
+    @PAINTFILL
+    D;JGT
+  @MAINLOOP
+  0;JMP
+
+(CLEAR)
+  @i
+  M=0
+  (PAINTCLEAR)
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    A=D
+    M=0
+    @i
+    M=M+1
+    @8192
+    D=A
+    @i
+    D=D-M
+    @PAINTCLEAR
+    D;JGT
+  @MAINLOOP
+  0;JMP

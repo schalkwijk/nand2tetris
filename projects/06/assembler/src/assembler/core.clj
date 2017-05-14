9ca4a0ea602d5472553408ae22095541a510744d
Samuel Lopes <samlopes89@gmail.com>
2017-05-14 00:16:53 -0500

    Assembling Add.asm without a problem

--- projects/06/assembler/src/assembler/core.clj
+++ projects/06/assembler/src/assembler/core.clj
@@ -1,12 +1,14 @@
 (ns assembler.core
   (:require [clojure.string :as str]))
 
+;; a instruction
 (defn- decimal-to-binary [decimal]
   (str/replace (format "%15s" (Integer/toString decimal 2)) " " "0"))
 
-(defn parse-a-instruction [& {:keys [raw]}]
+(defn- parse-a-instruction [raw]
   {:instruction (str "0" (decimal-to-binary (Integer/parseInt (str/replace-first raw "@" ""))))})
 
+;; c instruction
 (defn- parse-c-cmp [cmp]
   (case cmp
     "A" "0110000"
@@ -21,10 +23,19 @@
 (defn- parse-c-jump [jump]
   "000")
 
-(defn parse-c-instruction [& {:keys [raw]}]
+(defn- parse-c-instruction [raw]
   (let [[dest cmp] (str/split raw #"=")]
   {:instruction (str "111" (parse-c-cmp cmp) (parse-c-dest dest) (parse-c-jump "filler"))}))
 
+;; main
+(defn parse-instruction [& {:keys [raw]}]
+  (if (= (subs raw 0 1) "@")
+    (parse-a-instruction raw)
+    (parse-c-instruction raw)))
+
+(defn assemble [instructions]
+  (map #(:instruction (parse-instruction :raw %)) instructions))
+
 (defn -main [file]
   (with-open [rdr (clojure.java.io/reader file)]
-    (print ((count (line-seq rdr))))))
+    (dorun (map println (assemble (line-seq rdr))))))
--- projects/06/assembler/test/assembler/core_test.clj
+++ projects/06/assembler/test/assembler/core_test.clj
@@ -3,11 +3,14 @@
              [assembler.core :refer :all]))
 
 (deftest assembling-a-instructions
-  (is (= "0000000000000010" (:instruction (parse-a-instruction :raw "@2"))))
-  (is (= "0000000000000011" (:instruction (parse-a-instruction :raw "@3"))))
-  (is (= "0000000000000000" (:instruction (parse-a-instruction :raw "@0")))))
+  (is (= "0000000000000010" (:instruction (parse-instruction :raw "@2"))))
+  (is (= "0000000000000011" (:instruction (parse-instruction :raw "@3"))))
+  (is (= "0000000000000000" (:instruction (parse-instruction :raw "@0")))))
 
 (deftest assembling-c-instructions
-  (is (= "1110110000010000" (:instruction (parse-c-instruction :raw "D=A"))))
-  (is (= "1110000010010000" (:instruction (parse-c-instruction :raw "D=D+A"))))
-  (is (= "1110001100001000" (:instruction (parse-c-instruction :raw "M=D")))))
+  (is (= "1110110000010000" (:instruction (parse-instruction :raw "D=A"))))
+  (is (= "1110000010010000" (:instruction (parse-instruction :raw "D=D+A"))))
+  (is (= "1110001100001000" (:instruction (parse-instruction :raw "M=D")))))
+
+(deftest assemble-returns-array-of-instructions
+  (is (= ["0000000000000010" "1110110000010000" "0000000000000011"] (assemble (seq ["@2" "D=A" "@3"])))))

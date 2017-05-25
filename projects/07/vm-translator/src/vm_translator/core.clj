4d36f4046e70fb9a748b8dd201015058ae1da31d
Samuel Lopes <samlopes89@gmail.com>
2017-05-24 19:19:48 -0500

    VMTranslator working for basic test

--- projects/07/vm-translator/src/vm_translator/core.clj
+++ projects/07/vm-translator/src/vm_translator/core.clj
@@ -1,5 +1,6 @@
 (ns vm-translator.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:gen-class))
 
 (defn- instruction-is-not-comment [instruction]
   (not (-> instruction
@@ -15,33 +16,46 @@
 (defn- generate-constant-push-command [value]
   (str/join "\n" [(str "@" value ) "D=A" "@SP" "A=M" "M=D" "@SP" "M=M+1"]))
 
+(defn- generate-relative-push-command [base-address location]
+  (str/join "\n" [(str "@" base-address ) "D=M" (str "@" location) "D=D+A" "A=D" "D=M" "@SP" "A=M" "M=D" "D=A+1" "@SP" "M=D"]))
+
+(defn- generate-temp-push-command [location]
+  (str/join "\n" ["@R5" "D=A" (str "@" location) "D=D+A" "A=D" "D=M" "@SP" "A=M" "M=D" "D=A+1" "@SP" "M=D"]))
+
 (defn- generate-push-command [memory-segment location]
   (case memory-segment
     "constant" (generate-constant-push-command location)
-    ;; (generate-relative-push-command memory-segment location)
-    ))
+    "local" (generate-relative-push-command "LCL" location)
+    "argument" (generate-relative-push-command "ARG" location)
+    "this" (generate-relative-push-command "THIS" location)
+    "that" (generate-relative-push-command "THAT" location)
+    "temp" (generate-temp-push-command location)))
 
 ;; pop commands
-(defn- generate-relative-pop-command [pointer location]
-  (str/join "\n" ["@SP" "M=M-1" (str "@" pointer) "D=M" (str "@" location) "D=D+A" "@R13" "M=D" "@SP" "A=M" "D=M" "@R13" "A=M" "M=D"]))
+(defn- generate-relative-pop-command [base-address location]
+  (str/join "\n" ["@SP" "M=M-1" (str "@" base-address) "D=M" (str "@" location) "D=D+A" "@R13" "M=D" "@SP" "A=M" "D=M" "@R13" "A=M" "M=D"]))
+
+(defn- generate-temp-pop-command [location]
+  (str/join "\n" ["@SP" "M=M-1" "@R5" "D=A" (str "@" location) "D=D+A" "@R13" "M=D" "@SP" "A=M" "D=M" "@R13" "A=M" "M=D"]))
 
 (defn- generate-pop-command [memory-segment location]
   (case memory-segment
     "local" (generate-relative-pop-command "LCL" location)
     "argument" (generate-relative-pop-command "ARG" location)
     "this" (generate-relative-pop-command "THIS" location)
-    "that" (generate-relative-pop-command "THAT" location)))
+    "that" (generate-relative-pop-command "THAT" location)
+    "temp" (generate-temp-pop-command location)))
 
-(defn- generate-sub-command [])
-(defn- generate-add-command [])
+(defn- generate-add-sub-command [operand]
+  (str/join "\n" ["@SP" "A=M-1" "D=M" "A=A-1" "A=M" (str "D=A" operand "D") "@SP" "A=M" "A=A-1" "A=A-1" "M=D" "D=A+1" "@SP" "M=D"]))
 
 (defn- parse-instruction [instruction]
   (let [split-args (str/split instruction #" ")]
         (case (first split-args)
           "pop" (apply generate-pop-command (rest split-args))
           "push" (apply generate-push-command (rest split-args))
-          "add" (generate-add-command)
-          "sub" (generate-sub-command))))
+          "add" (generate-add-sub-command "+")
+          "sub" (generate-add-sub-command "-"))))
 
 ;; main
 (defn translate [instructions]
@@ -51,3 +65,8 @@
 (defn -main [file]
   (with-open [rdr (clojure.java.io/reader file)]
     (dorun (map println (translate (line-seq rdr))))))
+
+
+;; todo
+;; support add
+;; support sub

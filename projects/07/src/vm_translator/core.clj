0285ac92c4e59d9797ce6ae8bb80530dd417c92b
Samuel Lopes <samlopes89@gmail.com>
2017-05-24 19:34:13 -0500

    Move VMTranslator things around a bit

---
+++ projects/07/src/vm_translator/core.clj
@@ -0,0 +1,68 @@
+(ns vm-translator.core
+  (:require [clojure.string :as str] [clojure.java.io :as io])
+  (:gen-class))
+
+(defn- instruction-is-not-comment [instruction]
+  (not (-> instruction
+           (str/split #"//")
+           first
+           str/trim
+           str/blank?)))
+
+(defn- strip-out-comments-and-whitespace [instructions]
+  (map #(str/trim (first (str/split % #"//"))) (filter instruction-is-not-comment instructions)))
+
+;; push commands
+(defn- generate-constant-push-command [value]
+  (str/join "\n" [(str "@" value ) "D=A" "@SP" "A=M" "M=D" "@SP" "M=M+1"]))
+
+(defn- generate-relative-push-command [base-address location]
+  (str/join "\n" [(str "@" base-address ) "D=M" (str "@" location) "D=D+A" "A=D" "D=M" "@SP" "A=M" "M=D" "D=A+1" "@SP" "M=D"]))
+
+(defn- generate-temp-push-command [location]
+  (str/join "\n" ["@R5" "D=A" (str "@" location) "D=D+A" "A=D" "D=M" "@SP" "A=M" "M=D" "D=A+1" "@SP" "M=D"]))
+
+(defn- generate-push-command [memory-segment location]
+  (case memory-segment
+    "constant" (generate-constant-push-command location)
+    "local" (generate-relative-push-command "LCL" location)
+    "argument" (generate-relative-push-command "ARG" location)
+    "this" (generate-relative-push-command "THIS" location)
+    "that" (generate-relative-push-command "THAT" location)
+    "temp" (generate-temp-push-command location)))
+
+;; pop commands
+(defn- generate-relative-pop-command [base-address location]
+  (str/join "\n" ["@SP" "M=M-1" (str "@" base-address) "D=M" (str "@" location) "D=D+A" "@R13" "M=D" "@SP" "A=M" "D=M" "@R13" "A=M" "M=D"]))
+
+(defn- generate-temp-pop-command [location]
+  (str/join "\n" ["@SP" "M=M-1" "@R5" "D=A" (str "@" location) "D=D+A" "@R13" "M=D" "@SP" "A=M" "D=M" "@R13" "A=M" "M=D"]))
+
+(defn- generate-pop-command [memory-segment location]
+  (case memory-segment
+    "local" (generate-relative-pop-command "LCL" location)
+    "argument" (generate-relative-pop-command "ARG" location)
+    "this" (generate-relative-pop-command "THIS" location)
+    "that" (generate-relative-pop-command "THAT" location)
+    "temp" (generate-temp-pop-command location)))
+
+(defn- generate-add-sub-command [operand]
+  (str/join "\n" ["@SP" "A=M-1" "D=M" "A=A-1" "A=M" (str "D=A" operand "D") "@SP" "A=M" "A=A-1" "A=A-1" "M=D" "D=A+1" "@SP" "M=D"]))
+
+(defn- parse-instruction [instruction]
+  (let [split-args (str/split instruction #" ")]
+        (case (first split-args)
+          "pop" (apply generate-pop-command (rest split-args))
+          "push" (apply generate-push-command (rest split-args))
+          "add" (generate-add-sub-command "+")
+          "sub" (generate-add-sub-command "-"))))
+
+;; main
+(defn translate [instructions]
+  (let [cleaned-instructions (strip-out-comments-and-whitespace instructions)]
+    (map #(parse-instruction %) cleaned-instructions)))
+
+(defn -main [file]
+  (with-open [rdr (io/reader file)]
+    (with-open [wrtr (io/writer (str/replace file ".vm" ".asm"))]
+      (dorun (map #(.write wrtr (str % "\n")) (translate (line-seq rdr)))))))
--- projects/07/vm-translator/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject vm-translator "0.1.0-SNAPSHOT"
-  :description "VM, Assemble!"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main vm-translator.core)

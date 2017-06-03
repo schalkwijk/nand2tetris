fef457187db9c6c6eec550fc6f213c3a37d9e892
Samuel Lopes <samlopes89@gmail.com>
2017-06-02 21:47:11 -0500

    Everything but string constants

--- projects/10/analyzer/src/analyzer/core.clj
+++ projects/10/analyzer/src/analyzer/core.clj
@@ -3,19 +3,25 @@
             [clojure.java.io :as io])
   (:gen-class))
 
-(defn- split-on-spaces [instruction]
-  (str/split instruction #"\s"))
-
 (def keywords
   ["class" "constructor" "function" "method" "field" "static" "var" "int" "char" "boolean" "void" "true" "false" "null" "this" "let" "do" "if" "else" "while" "return"])
 
 (def symbols
   ["{" "}" "(" ")" "[" "]" "." "," ";" "+" "-" "*" "/" "&" "|" "<" ">" "=" "~"])
 
-(defn in?
+(def integer-re
+  #"[0-9]+(.[0-9]+)?")
+
+(defn- in?
   "true if collection contains element"
   [collection element]
   (some #(= element %) collection))
+
+(defn- void-matcher [pattern]
+  (str "(?=\\" pattern ")|(?<=\\" pattern ")"))
+
+(defn- split-on-delimiters [instruction]
+  (filter #(not (str/blank? %)) (str/split instruction (re-pattern (str/join "|" (conj (map void-matcher symbols) "\\s"))))))
 
 (defn- construct-keyword [bit]
   (if (in? keywords bit) {:type :keyword :value bit}))
@@ -23,16 +29,27 @@
 (defn- construct-symbol [bit]
   (if (in? symbols bit) {:type :symbol :value bit}))
 
+(defn- construct-integer [bit]
+  (if (re-matches integer-re bit) {:type :integerConstant :value bit}))
+
 (defn- construct-identifier [bit]
   {:type :identifier :value bit})
 
 (defn- identify-token [bit]
-  (first (filter #(not (nil? %)) (map #(% bit) [construct-keyword construct-symbol construct-identifier]))))
+  (first (filter #(not (nil? %)) (map #(% bit) [construct-keyword construct-symbol construct-integer construct-identifier]))))
 
 (defn analyze [instruction]
-  (let [instruction-bits (split-on-spaces instruction)]
+  (let [{interned-strings :interned-strings
+         interned-instruction :interned-instruction} (intern-string-constants instruction)
+        instruction-bits (split-on-delimiters interned-instruction)]
     (map identify-token instruction-bits)))
 
 ;; (defn -main [file]
 ;;   (with-open [rdr (clojure.java.io/reader file)]
 ;;     (dorun (map println (analyze (line-seq rdr) file)))))
+
+;; (defn- intern-string [string-to-intern instruction]
+;;   )
+
+;; (defn- intern-string-constants [instruction]
+;;   (reduce #(intern-string %2 %1) {:interned-strings {} :interned-instruction instruction} (or (re-seq #"\"(.*?[^\\])\"" instruction) [])))
--- projects/10/analyzer/test/analyzer/core_test.clj
+++ projects/10/analyzer/test/analyzer/core_test.clj
@@ -8,3 +8,77 @@
     {:type :identifier :value "Test"} 1
     {:type :symbol :value "{"} 2
     {:type :symbol :value "}"} 3))
+
+(deftest handling-variable-assignment
+  (are [value location] (= value (nth (analyze "let i = sum") location))
+    {:type :keyword :value "let"} 0
+    {:type :identifier :value "i"} 1
+    {:type :symbol :value "="} 2
+    {:type :identifier :value "sum"} 3))
+
+(deftest handling-variable-assignment-with-function-call
+  (are [value location] (= value (nth (analyze "let i = Array.new(length);") location))
+    {:type :keyword :value "let"} 0
+    {:type :identifier :value "i"} 1
+    {:type :symbol :value "="} 2
+    {:type :identifier :value "Array"} 3
+    {:type :symbol :value "."} 4
+    {:type :identifier :value "new"} 5
+    {:type :symbol :value "("} 6
+    {:type :identifier :value "length"} 7
+    {:type :symbol :value ")"} 8
+    {:type :symbol :value ";"} 9))
+
+(deftest handling-function-declarations
+  (are [value location] (= value (nth (analyze "function void main() { }") location))
+    {:type :keyword :value "function"} 0
+    {:type :keyword :value "void"} 1
+    {:type :identifier :value "main"} 2
+    {:type :symbol :value "("} 3
+    {:type :symbol :value ")"} 4
+    {:type :symbol :value "{"} 5
+    {:type :symbol :value "}"} 6))
+
+(deftest handling-function-calling
+  (are [value location] (= value (nth (analyze "do Output.printInt(sum / length);") location))
+    {:type :keyword :value "do"} 0
+    {:type :identifier :value "Output"} 1
+    {:type :symbol :value "."} 2
+    {:type :identifier :value "printInt"} 3
+    {:type :symbol :value "("} 4
+    {:type :identifier :value "sum"} 5
+    {:type :symbol :value "/"} 6
+    {:type :identifier :value "length"} 7
+    {:type :symbol :value ")"} 8
+    {:type :symbol :value ";"} 9))
+
+(deftest handling-array-accessing
+  (are [value location] (= value (nth (analyze "let sum = sum + a[i]") location))
+    {:type :keyword :value "let"} 0
+    {:type :identifier :value "sum"} 1
+    {:type :symbol :value "="} 2
+    {:type :identifier :value "sum"} 3
+    {:type :symbol :value "+"} 4
+    {:type :identifier :value "a"} 5
+    {:type :symbol :value "["} 6
+    {:type :identifier :value "i"} 7
+    {:type :symbol :value "]"} 8))
+
+(deftest handling-integer-constants
+  (are [value location] (= value (nth (analyze "let i = 100;") location))
+    {:type :keyword :value "let"} 0
+    {:type :identifier :value "i"} 1
+    {:type :symbol :value "="} 2
+    {:type :integerConstant :value "100"} 3
+    {:type :symbol :value ";"} 4))
+
+(deftest handling-string-constants
+  (are [value location] (= value (nth (analyze "do Output.printString(\"THE AVERAGE IS: \")") location))
+    {:type :keyword :value "do"} 0
+    {:type :identifier :value "Output"} 1
+    {:type :symbol :value "."} 2
+    {:type :identifier :value "printString"} 3
+    {:type :symbol :value "("} 4
+    {:type :stringConstant :value "THE AVERAGE IS: "} 5
+    {:type :symbol :value ")"} 6
+    {:type :identifier :value ";"} 7))

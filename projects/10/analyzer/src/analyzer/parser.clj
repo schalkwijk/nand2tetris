7b735b1e5146a00990494d543ffa657ecddadce1
Samuel Lopes <samlopes89@gmail.com>
2017-06-03 18:06:25 -0500

    Simple class / method parser

---
+++ projects/10/analyzer/src/analyzer/parser.clj
@@ -0,0 +1,52 @@
+(ns analyzer.parser)
+
+(defn- in?
+  "True if collection contains element"
+  [collection element]
+  (some #(= element %) collection))
+
+(defn- raise [message]
+  (throw (Exception. message)))
+
+(defn- consume-without-value [type {:keys [tokens parsed-elements]}]
+  (let [next-token (first tokens)]
+    (if (= (:type next-token) type)
+      {:tokens (rest tokens) :parsed-elements (conj parsed-elements next-token)}
+      (raise (str "Expected " type ", got " (:type next-token) " with value " (:value next-token))))))
+
+(defn- consume [type value {:keys [tokens parsed-elements]}]
+  (let [next-token (first tokens)]
+    (if (= next-token {:type type :value value})
+      {:tokens (rest tokens) :parsed-elements (conj parsed-elements next-token)}
+      (raise (str "Expected " type " with value " value ", got " (:type next-token) " with value " (:value next-token))))))
+
+(defn- parse-class-var-dec [] nil)
+
+(defn- looking-at-keyword [potential-keywords tokens]
+  (let [next-token (first tokens)]
+    (and (= (:type next-token :keyword))
+         (in? potential-keywords (:value next-token)))))
+
+(defn- parse-subroutine [tokens]
+  (consume :symbol "}" (consume :symbol "{" (consume :symbol ")" (consume :symbol "(" (consume-without-value :identifier (consume-without-value :identifier (consume-without-value :keyword {:tokens tokens :parsed-elements []}))))))))
+
+(defn- parse-class-body [tokens parsed-elements]
+  (cond
+    (looking-at-keyword ["constructor" "function" "method"] tokens)
+    (let [subroutine (parse-subroutine tokens)]
+      (recur (:tokens subroutine) (conj parsed-elements {:subroutineDec (:parsed-elements subroutine)})))
+
+    (looking-at-keyword ["static" "field"] tokens) (parse-class-var-dec)
+    :else {:tokens tokens :parsed-elements parsed-elements}))
+
+(defn- parse-class [tokens]
+  (let [cross-roads (consume :symbol "{" (consume-without-value :identifier (consume :keyword "class" {:tokens tokens :parsed-elements []})))]
+    (if (looking-at-keyword ["constructor" "function" "method" "static" "field"] (:tokens cross-roads))
+      (:parsed-elements (consume :symbol "}" (parse-class-body (:tokens cross-roads) (:parsed-elements cross-roads))))
+      (raise "class body does not start with subroutine or variable declaration"))))
+
+(defn- handle-class [tokens]
+  [{:class (parse-class tokens)}])
+
+(defn parse-tokens [tokens]
+  (handle-class tokens))
---
+++ projects/10/analyzer/test/analyzer/parser_test.clj
@@ -0,0 +1,41 @@
+(ns analyzer.parser-test
+  (:require  [analyzer.parser :refer :all]
+             [clojure.test :refer :all]))
+
+(defn- create-token [type value]
+  {:type type :value value})
+
+(def class-t (create-token :keyword "class"))
+(def class-name (create-token :identifier "Main"))
+(def open-curly (create-token :symbol "{"))
+(def close-curly (create-token :symbol "}"))
+(def open-paren (create-token :symbol "("))
+(def close-paren (create-token :symbol ")"))
+(def method (create-token :keyword "method"))
+(def method-return-type (create-token :identifier "Fraction"))
+(def method-name (create-token :identifier "foo"))
+(def int-t (create-token :keyword "int"))
+(def method-parameter (create-token :keyword "y"))
+
+;; class Main { function Fraction foo() {} }
+(deftest handling-function-declarations
+  (let [tokens [class-t class-name open-curly method method-return-type method-name
+                open-paren close-paren open-curly close-curly close-curly]]
+    (are [path value] (= value (get-in (vec (parse-tokens tokens)) path))
+      [0 :class 0] class-t
+      [0 :class 1] class-name
+      [0 :class 2] open-curly
+      [0 :class 3 :subroutineDec 0] method
+      [0 :class 3 :subroutineDec 1] method-return-type
+      [0 :class 3 :subroutineDec 2] method-name
+      [0 :class 3 :subroutineDec 3] open-paren
+      [0 :class 3 :subroutineDec 4] close-paren
+      [0 :class 3 :subroutineDec 5] open-curly
+      [0 :class 3 :subroutineDec 6] close-curly
+      [0 :class 4] close-curly)))
+
+;; TODO
+;; print basic example to see if you can properly construct XML
+;; Handle void return type in method
+;; Handle variable declarations in class
+;; Handle method body

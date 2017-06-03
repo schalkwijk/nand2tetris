83e9747afee2bc79c0c240b15bd3b46815364f1a
Samuel Lopes <samlopes89@gmail.com>
2017-06-03 12:58:13 -0500

    Create Makefil

--- projects/10/analyzer/project.clj
+++ projects/10/analyzer/project.clj
@@ -1,4 +1,4 @@
-(defproject vm-translator "0.1.0-SNAPSHOT"
+(defproject analyzer "0.1.0-SNAPSHOT"
   :description "Jack code analyzer"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main analyzer.core)

838156496aa7c174572c7ccccc0972d11919fa77
Samuel Lopes <samlopes89@gmail.com>
2017-06-03 12:33:50 -0500

    Move things in preparation of parser

--- projects/10/analyzer/src/analyzer/core.clj
+++ projects/10/analyzer/src/analyzer/core.clj
@@ -1,79 +1,8 @@
 (ns analyzer.core
   (:require [clojure.string :as str]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [analyzer.tokenizer :refer :all])
   (:gen-class))
-
-(def keywords
-  ["class" "constructor" "function" "method" "field" "static" "var" "int" "char" "boolean" "void" "true" "false" "null" "this" "let" "do" "if" "else" "while" "return"])
-
-(def symbols
-  ["{" "}" "(" ")" "[" "]" "." "," ";" "+" "-" "*" "/" "&" "|" "<" ">" "=" "~"])
-
-(def integer-re
-  #"[0-9]+(.[0-9]+)?")
-
-(def html-escape-sequences
-  {\< "&lt;", \> "&gt;", \& "&amp;"})
-
-(defn- in?
-  "True if collection contains element"
-  [collection element]
-  (some #(= element %) collection))
-
-(defn- first-or-empty [instruction]
-  (if (empty? instruction) "" (first instruction)))
-
-(defn- instruction-is-not-comment [instruction]
-  (not (-> instruction
-           (str/split #"//|/\*\*|\*|\*/")
-           first-or-empty
-           str/trim
-           str/blank?)))
-
-(defn- split-and-strip-instructions [instructions]
-  (map #(first (str/split % #"//")) (filter instruction-is-not-comment instructions)))
-
-(defn- void-matcher [pattern]
-  (str "(?=\\" pattern ")|(?<=\\" pattern ")"))
-
-(defn- split-on-delimiters [instruction]
-  (filter #(not (str/blank? %)) (str/split instruction (re-pattern (str/join "|" (conj (map void-matcher symbols) "\\s"))))))
-
-(defn- construct-keyword [bit]
-  (if (in? keywords bit) {:type :keyword :value bit}))
-
-(defn- construct-symbol [bit]
-  (if (in? symbols bit) {:type :symbol :value bit}))
-
-(defn- construct-integer [bit]
-  (if (re-matches integer-re bit) {:type :integerConstant :value bit}))
-
-(defn- construct-string [interned-strings bit]
-  (if (contains? interned-strings bit) {:type :stringConstant :value (get interned-strings bit)}))
-
-(defn- construct-identifier [bit]
-  {:type :identifier :value bit})
-
-(defn- identify-token [bit interned-strings]
-  (first (filter some? (map #(% bit) [(partial construct-string interned-strings) construct-keyword construct-symbol construct-integer construct-identifier]))))
-
-(defn- intern-strings [interned-strings instruction]
-  (let [match (re-find #"\"(.*?[^\\])\"" instruction)
-        replacement-symbol (str "@" (count interned-strings))]
-    (if match (recur (assoc interned-strings replacement-symbol (last match)) (str/replace instruction (first match) replacement-symbol))
-        {:interned-strings interned-strings :instruction instruction})))
-
-(defn- intern-string-constants [instruction]
-  (intern-strings {} instruction))
-
-(defn analyze-instruction [instruction]
-  (let [{interned-strings :interned-strings
-         interned-instruction :instruction} (intern-string-constants instruction)
-        instruction-bits (split-on-delimiters interned-instruction)]
-    (map #(identify-token % interned-strings) instruction-bits)))
-
-(defn analyze-instructions [instructions]
-  (map analyze-instruction (split-and-strip-instructions instructions)))
 
 (defn- format-instruction [instruction]
   (let [type (name (:type instruction))
@@ -84,12 +13,12 @@
   (let [formatted-instructions (str/join "\n" (map format-instruction instructions))]
     (print (str "<tokens>\n" formatted-instructions "\n</tokens>"))))
 
-(defn- analyze-single-file [filename]
+(defn- tokenize-and-output-single-file [filename]
   (let [xml-filename (str/replace (.getName (io/file filename)) ".jack" "T.xml")
         output-filename (str (System/getProperty "user.dir") "/" xml-filename)]
     (with-open [rdr (io/reader filename)]
       (with-open [wrtr (io/writer output-filename)]
-        (.write wrtr (with-out-str (output-instructions (flatten (analyze-instructions (line-seq rdr))))))))))
+        (.write wrtr (with-out-str (output-instructions (flatten (tokenize-instructions (line-seq rdr))))))))))
 
 (defn- extract-jack-files-from-directory [directory]
   (filter #(re-matches #".*\.jack" (.getName %)) (.listFiles directory)))
@@ -98,4 +27,4 @@
   (let [file (io/file filename)
         directory? (.isDirectory file)
         files (if directory? (extract-jack-files-from-directory file) [file])]
-    (doall (map analyze-single-file files))))
+    (doall (map tokenize-and-output-single-file files))))
---
+++ projects/10/analyzer/test/analyzer/tokenizer_test.clj
@@ -0,0 +1,126 @@
+(ns analyzer.tokenizer-test
+  (:require [analyzer.tokenizer :refer :all]
+            [clojure.test :refer :all]))
+
+(deftest handling-class-declarations
+  (are [value location] (= value (nth (tokenize-instruction "class Test { }") location))
+    {:type :keyword :value "class"} 0
+    {:type :identifier :value "Test"} 1
+    {:type :symbol :value "{"} 2
+    {:type :symbol :value "}"} 3))
+
+(deftest handling-variable-assignment
+  (are [value location] (= value (nth (tokenize-instruction "let i = sum") location))
+    {:type :keyword :value "let"} 0
+    {:type :identifier :value "i"} 1
+    {:type :symbol :value "="} 2
+    {:type :identifier :value "sum"} 3))
+
+(deftest handling-variable-assignment-with-function-call
+  (are [value location] (= value (nth (tokenize-instruction "let i = Array.new(length);") location))
+    {:type :keyword :value "let"} 0
+    {:type :identifier :value "i"} 1
+    {:type :symbol :value "="} 2
+    {:type :identifier :value "Array"} 3
+    {:type :symbol :value "."} 4
+    {:type :identifier :value "new"} 5
+    {:type :symbol :value "("} 6
+    {:type :identifier :value "length"} 7
+    {:type :symbol :value ")"} 8
+    {:type :symbol :value ";"} 9))
+
+(deftest handling-function-declarations
+  (are [value location] (= value (nth (tokenize-instruction "function void main() { }") location))
+    {:type :keyword :value "function"} 0
+    {:type :keyword :value "void"} 1
+    {:type :identifier :value "main"} 2
+    {:type :symbol :value "("} 3
+    {:type :symbol :value ")"} 4
+    {:type :symbol :value "{"} 5
+    {:type :symbol :value "}"} 6))
+
+(deftest handling-function-calling
+  (are [value location] (= value (nth (tokenize-instruction "do Output.printInt(sum / length);") location))
+    {:type :keyword :value "do"} 0
+    {:type :identifier :value "Output"} 1
+    {:type :symbol :value "."} 2
+    {:type :identifier :value "printInt"} 3
+    {:type :symbol :value "("} 4
+    {:type :identifier :value "sum"} 5
+    {:type :symbol :value "/"} 6
+    {:type :identifier :value "length"} 7
+    {:type :symbol :value ")"} 8
+    {:type :symbol :value ";"} 9))
+
+(deftest handling-array-accessing
+  (are [value location] (= value (nth (tokenize-instruction "let sum = sum + a[i]") location))
+    {:type :keyword :value "let"} 0
+    {:type :identifier :value "sum"} 1
+    {:type :symbol :value "="} 2
+    {:type :identifier :value "sum"} 3
+    {:type :symbol :value "+"} 4
+    {:type :identifier :value "a"} 5
+    {:type :symbol :value "["} 6
+    {:type :identifier :value "i"} 7
+    {:type :symbol :value "]"} 8))
+
+(deftest handling-integer-constants
+  (are [value location] (= value (nth (tokenize-instruction "let i = 100;") location))
+    {:type :keyword :value "let"} 0
+    {:type :identifier :value "i"} 1
+    {:type :symbol :value "="} 2
+    {:type :integerConstant :value "100"} 3
+    {:type :symbol :value ";"} 4))
+
+(deftest handling-string-constants
+  (are [value location] (= value (nth (tokenize-instruction "do Output.printString(\"THE AVERAGE IS: \");") location))
+    {:type :keyword :value "do"} 0
+    {:type :identifier :value "Output"} 1
+    {:type :symbol :value "."} 2
+    {:type :identifier :value "printString"} 3
+    {:type :symbol :value "("} 4
+    {:type :stringConstant :value "THE AVERAGE IS: "} 5
+    {:type :symbol :value ")"} 6
+    {:type :symbol :value ";"} 7))
+
+(deftest handling-multiline-entries
+  (let [instructions ["class Main {", "function void main() {", " var Array a;", "}"]]
+    (are [value instruction-number token-number]
+        (= value (nth (nth (tokenize-instructions instructions) instruction-number) token-number))
+        {:type :keyword :value "class"} 0 0
+        {:type :identifier :value "Main"} 0 1
+        {:type :symbol :value "{"} 0 2
+        {:type :keyword :value "function"} 1 0
+        {:type :keyword :value "void"} 1 1
+        {:type :identifier :value "main"} 1 2
+        {:type :symbol :value "("} 1 3
+        {:type :symbol :value ")"} 1 4
+        {:type :symbol :value "{"} 1 5
+        {:type :keyword :value "var"} 2 0
+        {:type :identifier :value "Array"} 2 1
+        {:type :identifier :value "a"} 2 2
+        {:type :symbol :value ";"} 2 3
+        {:type :symbol :value "}"} 3 0)))
+
+(deftest handling-comments-at-beginning-of-line
+  (let [instructions ["//Test", "/** Longer Comment */", "class Main {", "function void main() {", "var Array a;", "}"]]
+    (are [value instruction-number token-number]
+        (= value (nth (nth (tokenize-instructions instructions) instruction-number) token-number))
+      {:type :keyword :value "class"} 0 0
+      {:type :identifier :value "Main"} 0 1
+      {:type :symbol :value "{"} 0 2
+      {:type :keyword :value "function"} 1 0
+      {:type :keyword :value "void"} 1 1
+      {:type :identifier :value "main"} 1 2
+      {:type :symbol :value "("} 1 3
+      {:type :symbol :value ")"} 1 4
+      {:type :symbol :value "{"} 1 5
+      {:type :keyword :value "var"} 2 0
+      {:type :identifier :value "Array"} 2 1
+      {:type :identifier :value "a"} 2 2
+      {:type :symbol :value ";"} 2 3
+      {:type :symbol :value "}"} 3 0)))
+
+(deftest handling-comments-after-code
+  (let [instructions ["class Main {", "function void main() {", " var Array a; // random var", "}"]]
+    (is (= 4 (count (nth (tokenize-instructions instructions) 2))))))

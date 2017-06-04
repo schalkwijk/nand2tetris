50e8e0e793a944cad3a2db78f4d1f9839d110085
Samuel Lopes <samlopes89@gmail.com>
2017-06-04 15:49:56 -0500

    Add test around outputting class var / function args

--- projects/10/analyzer/test/analyzer/formatter_test.clj
+++ projects/10/analyzer/test/analyzer/formatter_test.clj
@@ -44,3 +44,45 @@
       [:content 3 :content 6 :content 0] " } "
       [:content 4 :tag] :symbol
       [:content 4 :content 0] " } ")))
+
+(deftest outputting-class-variables
+  (let [jack-code ["class Main { field int foo, bar; }"]
+        parse-tree (parse-tokens (tokenize-instructions jack-code))]
+    (are [location value]
+        (= value (get-in (parse-xml (with-out-str (output-parse-tree parse-tree))) location))
+      [:content 3 :tag] :classVarDec
+      [:content 3 :content 0 :tag] :keyword
+      [:content 3 :content 0 :content 0] " field "
+      [:content 3 :content 1 :tag] :keyword
+      [:content 3 :content 1 :content 0] " int "
+      [:content 3 :content 2 :tag] :identifier
+      [:content 3 :content 2 :content 0] " foo "
+      [:content 3 :content 3 :tag] :symbol
+      [:content 3 :content 3 :content 0] " , "
+      [:content 3 :content 4 :tag] :identifier
+      [:content 3 :content 4 :content 0] " bar ")))
+
+(deftest outputting-method-variables
+  (let [jack-code ["class Main { method void foo(int bar, Fraction baz) {} }"]
+        parse-tree (parse-tokens (tokenize-instructions jack-code))]
+    (are [location value]
+        (= value (get-in (parse-xml (with-out-str (output-parse-tree parse-tree))) location))
+      [:tag] :class
+      [:content 0 :tag] :keyword
+      [:content 0 :content 0] " class "
+      [:content 1 :tag] :identifier
+      [:content 1 :content 0] " Main "
+      [:content 2 :tag] :symbol
+      [:content 2 :content 0] " { "
+      [:content 3 :tag] :subroutineDec
+      [:content 3 :content 4 :tag] :parameterList
+      [:content 3 :content 4 :content 0 :tag] :keyword
+      [:content 3 :content 4 :content 0 :content 0] " int "
+      [:content 3 :content 4 :content 1 :tag] :identifier
+      [:content 3 :content 4 :content 1 :content 0] " bar "
+      [:content 3 :content 4 :content 2 :tag] :symbol
+      [:content 3 :content 4 :content 2 :content 0] " , "
+      [:content 3 :content 4 :content 3 :tag] :identifier
+      [:content 3 :content 4 :content 3 :content 0] " Fraction "
+      [:content 3 :content 4 :content 4 :tag] :identifier
+      [:content 3 :content 4 :content 4 :content 0] " baz ")))

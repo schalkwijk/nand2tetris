56d3a6837f010031555bb19036ec076491a5a875
Samuel Lopes <samlopes89@gmail.com>
2017-06-10 09:27:28 -0500

    Working Seven.jack program

--- projects/11/analyzer/src/analyzer/code_generator.clj
+++ projects/11/analyzer/src/analyzer/code_generator.clj
@@ -4,6 +4,8 @@
             [clojure.zip :as zip]
             [clojure.string :as str]))
 
+(declare compile-expression)
+
 (defn- zip-str [s]
   (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
@@ -11,15 +13,46 @@
   (let [consumed (if consumed consumed [])
         current-node-content (zip/node (zip/down zipper))]
     (if (= value current-node-content)
-      {:zipper zipper :consumed consumed}
+      {:zipper (zip/right zipper) :consumed consumed}
       (recur value (zip/right zipper) (conj consumed current-node-content)))))
+
+(defn- compile-term [zipper]
+  (let [type (:tag (zip/node zipper))
+        content (zip/node (zip/down zipper))]
+    (cond
+      (= type :integerConstant)
+      [(writer/write-constant-push content)]
+
+      (and (= content "(") (= type :symbol))
+      (compile-expression (zip/down (zip/right zipper)) [])
+      :else zipper
+      )))
+
+(defn- compile-expression [zipper commands]
+  (if (nil? zipper)
+    commands
+    (let [type (:tag (zip/node zipper))]
+     (cond
+       (= type :term)
+       (recur (zip/right zipper) (concat commands (compile-term (zip/down zipper))))
+
+       (= type :symbol)
+       (concat commands (compile-expression (zip/right zipper) []) [(writer/write-operator (zip/node (zip/down zipper)))])
+
+       :else commands))))
+
+(defn- compile-expression-list [zipper]
+  (reduce #(concat %1 (compile-expression (zip/down (zip/xml-zip %2)) [])) [] (zip/children zipper)))
 
 (defn- compile-do-statement [do-statement]
   (let [{zipper :zipper function-call :consumed} (consume-content-until-value "(" (zip/right do-statement))]
-    [(writer/write-subroutine-call (str/join function-call) (count (zip/children zipper)))]))
+    (conj (vec (compile-expression-list zipper)) ;; cast to vec so conj behaves correctly
+            (writer/write-subroutine-call (str/join function-call) (count (zip/children zipper)))
+            (str "pop temp 0")))) ;; if it's a do statement, we discard the return value
 
 (defn- compile-subroutine-body [commands subroutine-body]
   (let [statement (:tag (zip/node subroutine-body))]
+    (println (zip/node subroutine-body))
     (cond
       (= statement :doStatement)
       (let [do-statement-commands
@@ -27,7 +60,10 @@
                  zip/down ;; go into the do statement body
                  compile-do-statement
                  (concat commands))]
-        (recur do-statement-commands (zip/next subroutine-body)))
+        (recur do-statement-commands (zip/right subroutine-body)))
+
+      (= statement :returnStatement)
+      (concat commands [(writer/write-constant-push 0) "return"])
 
       :else commands)))
 

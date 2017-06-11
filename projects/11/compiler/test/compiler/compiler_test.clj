8fd21dae80574de34dcfff53255117739378dbda
Samuel Lopes <samlopes89@gmail.com>
2017-06-11 15:09:33 -0500

    Make compiler tests a bit more legible

--- projects/11/compiler/test/compiler/compiler_test.clj
+++ projects/11/compiler/test/compiler/compiler_test.clj
@@ -10,11 +10,28 @@
   (with-out-str (output-parse-tree (parse-tokens (tokenize-instructions instructions)))))
 
 (deftest simple-seven-program
-  (let [expected-output ["function Main.main 0" "push constant 1" "push constant 2" "push constant 3" "call Math.multiply 2" "add" "call Output.printInt 1" "pop temp 0" "push constant 0" "return"]
-        instructions ["class Main {" "function void main() {" "do Output.printInt(1 + (2 * 3));" "return;" "}}"]]
-    (is (= expected-output (compile-code (format-tokens-for-compiler instructions))))))
+  (let [instructions ["class Main {" "function void main() {" "do Output.printInt(1 + (2 * 3));" "return;" "}}"]
+        output (compile-code (format-tokens-for-compiler instructions))]
+    (are [path value] (= (nth output path) value)
+        0 "function Main.main 0"
+        1 "push constant 1"
+        2 "push constant 2"
+        3 "push constant 3"
+        4 "call Math.multiply 2"
+        5 "add"
+        6 "call Output.printInt 1"
+        7 "pop temp 0"
+        8 "push constant 0"
+        9 "return")))
 
 (deftest subroutine-variable-dec-and-set
-  (let [expected-output ["function Test.main 1" "push constant 8000" "call Memory.peek 1" "pop local 0" "push local 0" "return"]
-        instructions ["class Test {" "function int main() {" "var int value;" "let value = Memory.peek(8000);" "return value;" "}}"]]
-    (is (= expected-output (compile-code (format-tokens-for-compiler instructions))))))
+  (let [instructions
+        ["class Test {" "function int main() {" "var int value;" "let value = Memory.peek(8000);" "return value;" "}}"]
+        output (compile-code (format-tokens-for-compiler instructions))]
+    (are [path value] (= (nth output path) value)
+      0 "function Test.main 1"
+      1 "push constant 8000"
+      2 "call Memory.peek 1"
+      3 "pop local 0"
+      4 "push local 0"
+      5 "return")))

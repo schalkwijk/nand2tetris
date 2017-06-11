58327eb516e8c9734a1a87d25ad052706351f460
Samuel Lopes <samlopes89@gmail.com>
2017-06-11 12:01:09 -0500

    Simple local variables, returns with values, and lets

--- projects/11/compiler/src/compiler/compiler.clj
+++ projects/11/compiler/src/compiler/compiler.clj
@@ -12,12 +12,16 @@
   (let [{segment :scope position :position} (st/get-symbol-by-name variable-name symbol-table)]
     (writer/write-segment-pop (name segment) position)))
 
-(defn- compile-subroutine-call [zipper]
+(defn- push-variable [variable-name symbol-table]
+  (let [{segment :scope position :position} (st/get-symbol-by-name variable-name symbol-table)]
+    (writer/write-segment-push (name segment) position)))
+
+(defn- compile-subroutine-call [symbol-table zipper]
   (let [{zipper :zipper function-call :consumed} (consume-content-until-value "(" zipper)]
-    (conj (vec (compile-expression-list zipper)) ;; cast to vec so conj behaves correctly
+    (conj (vec (compile-expression-list symbol-table zipper)) ;; cast to vec so conj behaves correctly
           (writer/write-subroutine-call (str/join function-call) (count (zip/children zipper))))))
 
-(defn- compile-term [zipper]
+(defn- compile-term [symbol-table zipper]
   (let [type (:tag (zip/node zipper))
         content (zip/node (zip/down zipper))]
     (cond
@@ -25,39 +29,49 @@
       [(writer/write-constant-push content)]
 
       (and (= content "(") (= type :symbol))
-      (compile-expression (zip/down (zip/right zipper)) [])
+      (compile-expression symbol-table (zip/down (zip/right zipper)) [])
 
-      (= type :identifier) ;; assume method call for now
-      (compile-subroutine-call zipper)
+      (= type :identifier)
+
+      (let [next-token (zip/right zipper)
+            token (if next-token (zip/node (zip/down next-token)) "")]
+        (if (or (= "(" token) (= "." token))
+          (compile-subroutine-call symbol-table zipper)
+          [(push-variable content symbol-table)]))
 
       :else zipper)))
 
-(defn- compile-expression [zipper commands]
+(defn- compile-expression [symbol-table zipper commands]
   (if (nil? zipper)
     commands
     (let [type (:tag (zip/node zipper))]
       (cond
         (= type :term)
-        (recur (zip/right zipper) (concat commands (compile-term (zip/down zipper))))
+        (recur symbol-table (zip/right zipper) (concat commands (compile-term symbol-table (zip/down zipper))))
 
         (= type :symbol)
-        (concat commands (compile-expression (zip/right zipper) []) [(writer/write-operator (zip/node (zip/down zipper)))])
+        (concat commands (compile-expression symbol-table (zip/right zipper) []) [(writer/write-operator (zip/node (zip/down zipper)))])
 
         :else commands))))
 
-(defn- compile-expression-list [zipper]
-  (reduce #(concat %1 (compile-expression (zip/down (zip/xml-zip %2)) [])) [] (zip/children zipper)))
+(defn- compile-expression-list [symbol-table zipper]
+  (reduce #(concat %1 (compile-expression symbol-table (zip/down (zip/xml-zip %2)) [])) [] (zip/children zipper)))
 
-(defn- compile-do-statement [do-statement]
+(defn- compile-do-statement [symbol-table do-statement]
   (let [{zipper :zipper function-call :consumed} (consume-content-until-value "(" (zip/right do-statement))]
-    (conj (vec (compile-expression-list zipper)) ;; cast to vec so conj behaves correctly
+    (conj (vec (compile-expression-list symbol-table zipper)) ;; cast to vec so conj behaves correctly
             (writer/write-subroutine-call (str/join function-call) (count (zip/children zipper)))
             (str "pop temp 0")))) ;; if it's a do statement, we discard the return value
 
 (defn- compile-let-statement [symbol-table l-statement]
   (let [{variable-name :value zipper :zipper} (zip-and-fetch-node-content l-statement [zip/right])
         {expression :value zipper :zipper} (zip-and-apply zipper [zip/right zip/right] [zip/down])]
-    (concat (compile-expression expression []) [(pop-into-variable variable-name symbol-table)])))
+    (concat (compile-expression symbol-table expression []) [(pop-into-variable variable-name symbol-table)])))
+
+(defn- compile-return [symbol-table return-statement]
+  (if (= 2 (count (zip/children return-statement))) ;; void return
+    [(writer/write-constant-push 0) "return"]
+    (conj (vec (compile-expression symbol-table (zip/down (zip/right (zip/down return-statement))) [])) "return")))
 
 (defn- compile-subroutine-body [commands symbol-table subroutine-body]
   (let [statement (:tag (zip/node subroutine-body))]
@@ -66,12 +80,14 @@
       (let [do-statement-commands
             (->> subroutine-body
                  zip/down ;; go into the do statement body
-                 compile-do-statement
+                 (compile-do-statement symbol-table)
                  (concat commands))]
         (recur do-statement-commands symbol-table (zip/right subroutine-body)))
 
       (= statement :returnStatement)
-      (concat commands [(writer/write-constant-push 0) "return"])
+      (->> subroutine-body
+           (compile-return symbol-table)
+           (concat commands))
 
       (= statement :letStatement)
       (let [l-statement-commands

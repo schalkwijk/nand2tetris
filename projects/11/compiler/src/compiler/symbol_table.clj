9421e1296a5e124df3ed4a0db4c5493be8c08882
Samuel Lopes <samlopes89@gmail.com>
2017-06-12 18:35:51 -0500

    Make symbol table a bit more flexible

--- projects/11/compiler/src/compiler/symbol_table.clj
+++ projects/11/compiler/src/compiler/symbol_table.clj
@@ -2,6 +2,9 @@
   (:require [clojure.zip :as zip]
             [compiler.zip-helpers :refer :all]))
 
+(def var-keyword-to-scope
+  {"var" :local})
+
 (defn get-symbol-by-name [symbol-name table]
   (first (filter #(= (:name %) symbol-name) table)))
 
@@ -17,20 +20,21 @@
       (recur (conj table {:name argument-name :type argument-type :position count :scope :argument})
              (zip/right zipper) (inc count)))))
 
-(defn- add-local-var-to-table [var-type var-name table]
+(defn- add-local-var-to-table [var-keyword var-type var-name table]
   (let [current-var-count (get-scope-variable-count :local table)]
-    (conj table {:name var-name :type var-type :position current-var-count :scope :local})))
+    (conj table {:name var-name :type var-type :position current-var-count :scope (get var-keyword-to-scope var-keyword)})))
 
 (defn- add-vars-to-table [original-table-size table zipper counter]
-  (let [type (zip/node (zip/down zipper))
-        vars-and-symbols (zip/rights zipper)]
-    (reduce #(add-local-var-to-table type (first (:content %2)) %1) table
+  (let [var-keyword (zip/node (zip/down zipper))
+        type (zip/node (zip/down (zip/right zipper)))
+        vars-and-symbols (zip/rights (zip/right zipper))]
+    (reduce #(add-local-var-to-table var-keyword type (first (:content %2)) %1) table
             (filter #(not (= (:tag %) :symbol)) vars-and-symbols))))
 
 (defn- recur-add-local-vars-to-table [table zipper counter original-table-size]
   (if (not (= :varDec (:tag (zip/node zipper))))
     {:symbol-table table :zipper zipper}
-    (let [var-zipper (zip/right (zip/down (zip/xml-zip (zip/node zipper))))
+    (let [var-zipper (zip/down (zip/xml-zip (zip/node zipper)))
           table (add-vars-to-table original-table-size table var-zipper counter)]
       (recur table (zip/right zipper) (count (filter #(= (:scope %) :local) table)) original-table-size))))
 
